2130757ce010888e2b2f6012ab468787c9e87477
lorenzobarilla <37076142+lorenzobarilla@users.noreply.github.com>
2019-10-30 00:44:27 +0100

    assignment + cover

--- sketch.js
+++ sketch.js
@@ -1,11 +1,51 @@
-function preload(){
+//declaring global variables
+var allMyLetters = []; //empty array
+var amountOfLetters = 15;
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+function preload() {
   // put preload code here
 }
 
 function setup() {
-  // put setup code here
+  noCanvas();
+
+  for (var i = 0; i < amountOfLetters; i++) {
+    var tempLetter = new Letter();
+    allMyLetters.push(tempLetter);
+  }
 }
 
 function draw() {
   // put drawing code here
+  for (var i = 0; i < allMyLetters.length; i++) {
+    var tempLetter = allMyLetters[i];
+    tempLetter.display();
+    //console.log("i = " + i);
+    //console.log(allMyLetters.length);
+  }
+
+  //html element modify using DOM
+  var myMessage = select('#message');
+  myMessage.style("font-family: Inconsolata; user-select: none;  color: green; background-color: white; padding: 25px; position: absolute; width: 225px; margin: 0 auto; top: 50%; left: 50%; transform: translate(-50%, -50%); z-index: 2;");
+}
+
+//creating the object
+function Letter() {
+  this.display = function() {
+    //html = letters array elements
+    var index = round(random(letters.length));
+    var letter = createElement('div', letters[index]);
+    //css style
+    letter.position(random(0, windowWidth), random(0, windowHeight));
+    letter.style("font-family: Inconsolata; user-select: none;  color: green;");
+    letter.style('font-size', 5 + mouseX / 15 + 'px');
+    //callback
+    letter.mouseClicked(letterClicked);
+  }
+}
+
+//callback function
+function letterClicked() {
+  this.style('color:red; font-size: 150px;');
 }
